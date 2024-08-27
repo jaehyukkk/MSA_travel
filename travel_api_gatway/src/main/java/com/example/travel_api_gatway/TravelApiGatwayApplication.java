0d4306d8ac174fe62b8b9adac98bcc1ffc7df594
jaehyuk home <wogur1178@naver.com>
2024-08-27 12:35:35 +0900

    refactor: 쿠버네티스 내부 디스커버리 사용으로 인한 유레카 설정 제거

--- travel_api_gatway/src/main/java/com/example/travel_api_gatway/TravelApiGatwayApplication.java
+++ travel_api_gatway/src/main/java/com/example/travel_api_gatway/TravelApiGatwayApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class TravelApiGatwayApplication {
 
 	public static void main(String[] args) {
