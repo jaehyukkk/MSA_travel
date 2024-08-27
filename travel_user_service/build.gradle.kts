56ba9fb742997c33aafda05d2b559843e2148e4d
jaehyuk home <wogur1178@naver.com>
2024-08-27 12:57:41 +0900

    refactor: 쿠버네티스 내부 디스커버리 사용으로 인한 유레카 제거

--- travel_user_service/build.gradle.kts
+++ travel_user_service/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation ("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.6")
+//	implementation ("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:3.1.6")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 
 	// 코틀린 lazy 로딩 이슈 관련
@@ -60,8 +60,6 @@ dependencies {
 
 	//redis
 	implementation ("org.springframework.boot:spring-boot-starter-data-redis")
-
-
 
 	//json
 	implementation("com.google.code.gson:gson:2.10.1")
