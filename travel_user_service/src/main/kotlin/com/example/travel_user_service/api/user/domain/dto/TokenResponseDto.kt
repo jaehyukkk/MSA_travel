7e8248321d281cc0923777a0d69d221d26116ae3
jaehyuk home <wogur1178@naver.com>
2024-08-27 12:42:24 +0900

    feat: 토큰 검증을 위한 validation 메서드 추가

--- travel_user_service/src/main/kotlin/com/example/travel_user_service/api/user/domain/dto/TokenResponseDto.kt
+++ travel_user_service/src/main/kotlin/com/example/travel_user_service/api/user/domain/dto/TokenResponseDto.kt
@@ -3,7 +3,7 @@ package com.example.travel_user_service.api.user.domain.dto
 class TokenResponseDto(
     val grantType: String,
     val accessToken: String,
-    val refreshToken: String? = null,
+    var refreshToken: String? = null,
     val accessTokenExpiresIn: Long,
     val refreshTokenExpiresIn: Long? = null
 ) {
--- travel_user_service/src/main/kotlin/com/example/travel_user_service/provider/TokenProvider.kt
+++ travel_user_service/src/main/kotlin/com/example/travel_user_service/provider/TokenProvider.kt
@@ -1,17 +1,22 @@
 package com.example.travel_user_service.provider
 
 import com.example.travel_user_service.api.user.domain.dto.TokenResponseDto
-import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import com.example.travel_user_service.exception.BaseException
+import com.example.travel_user_service.exception.ErrorCode
+import io.jsonwebtoken.*
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 import java.security.Key
 import java.util.*
 import java.util.stream.Collectors
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 @Component
 class TokenProvider(
@@ -64,6 +69,53 @@ class TokenProvider(
             accessTokenExpiresIn = accessTokenExpiresIn.time,
             refreshTokenExpiresIn = refreshTokenExpiresIn.time
         )
+    }
+
+    fun validateToken(token: String): Boolean {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)
+            return true
+        } catch (e: io.jsonwebtoken.security.SecurityException) {
+            throw JwtException("잘못된 JWT 서명입니다.")
+
+        } catch (e: MalformedJwtException) {
+            throw JwtException("잘못된 JWT 서명입니다.")
+
+        } catch (e: ExpiredJwtException) {
+            throw JwtException("만료된 JWT 토큰입니다.")
+
+        } catch (e: UnsupportedJwtException) {
+            throw JwtException("지원되지 않는 JWT 토큰입니다.")
+
+        } catch (e: IllegalArgumentException) {
+            throw JwtException("지원되지 않는 JWT 토큰입니다.")
+        }
+    }
+
+    fun getAuthentication(accessToken: String): Authentication {
+        val claims = parseClaims(accessToken)
+
+        if (claims[AUTHORITIES_KEY] == null) {
+            throw BaseException(ErrorCode.FORBIDDEN)
+        }
+
+        val authorities = claims[AUTHORITIES_KEY]
+            .toString()
+            .split(",")
+            .map(::SimpleGrantedAuthority)
+            .toList()
+
+        val principal: UserDetails = User(claims.subject, "", authorities)
+
+        return UsernamePasswordAuthenticationToken(principal, "", authorities)
+    }
+
+    fun parseClaims(accessToken: String): Claims {
+        return try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).body
+        } catch (e: ExpiredJwtException) {
+            e.claims
+        }
     }
 }
 
